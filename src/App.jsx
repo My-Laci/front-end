272ca9ba323981c07b9e1bb840ae0d7dfec94f68
marcaroon <ammarqorni@gmail.com>
2024-07-22 11:38:05 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
-function App() {
+const loadGoogleFont = () => {
+  const link = document.createElement("link");
+  link.href =
+    "https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap";
+  link.rel = "stylesheet";
+  document.head.appendChild(link);
+};
 
+function App() {
+  useEffect;
+  () => {
+    loadGoogleFont;
+  };
   return <Navbar />;
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
+import React from "react";
+import "../styles/Navbar.css";
+
 const Navbar = () => {
   return (
-    <header>
-      <div class="logo">Laci</div>
-      <div class="search">
+    <header className="header">
+      <div className="logo-sidebar">
+        <img src="src/assets/burger.svg" />
+        <div className="logo">Laci</div>
+      </div>
+      <div className="search-container">
+        <img src="src/assets/search-in.svg" />
         <input type="text" placeholder="Search" />
       </div>
-      <div class="user-info">
-        <div class="user-name">Edo Mahendra</div>
-        <div class="user-university">Universitas Airlangga</div>
-      </div>
-      <div class="user-image">
-        <img src="sakil.png" alt="User Image" />
+      <div className="user-section">
+        <div className="user-info">
+          <div className="user-name">Edo Mahendra</div>
+          <div className="user-university">Universitas Airlangga</div>
+        </div>
+        <div className="search-icon">
+          <img src="src/assets/search.svg" />
+        </div>
+        <div className="add-post-article">
+          <img src="src/assets/add.svg" />
+        </div>
+        <div className="user-image">
+          <img src="src/assets/sakil.png" alt="User Image" />
+        </div>
       </div>
     </header>
   );
