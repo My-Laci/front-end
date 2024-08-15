d280f905af653ca8ae785898044176d0d8f6b58c
marcaroon <ammarqorni@gmail.com>
2024-08-15 14:37:13 +0700

    fix: admin page fix

--- src/components/admin-sidebar/admin-sidebar.jsx
+++ src/components/admin-sidebar/admin-sidebar.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import "./admin-sidebar.css";
-import CreateVoucherIcon from "../../assets/voucher.svg";
-import HistoryIcon from "../../assets/history.svg";
 import CheckIcon from "../../assets/check.svg";
-=======
-import "./admin-sidebar.css";
 import CreateVoucherIcon from "../../assets/voucher.svg";
 import HistoryIcon from "../../assets/history.svg";
->>>>>>> 920f0ff71a649808ebdb0fbdff658a19d1295f3c
 import Logo from "../../assets/Laci.svg";
+import "./admin-sidebar.css";
 
 export default function AdminSidebar() {
   return (
@@ -17,7 +11,6 @@ export default function AdminSidebar() {
       <div className="logo">
         <img src={Logo} alt="Laci Logo" />
       </div>
-<<<<<<< HEAD
       <Link to="/AdminCreateVoucher" className="nav-link">
         <button>
           <img src={CreateVoucherIcon} alt="Edit" className="edit-icon" />
@@ -36,16 +29,6 @@ export default function AdminSidebar() {
           History
         </button>
       </Link>
-=======
-      <button>
-        <img src={CreateVoucherIcon} alt="Edit" className="edit-icon" />
-        Create Voucher
-      </button>
-      <button>
-        <img src={HistoryIcon} alt="Edit" className="edit-icon" />
-        History
-      </button>
->>>>>>> 920f0ff71a649808ebdb0fbdff658a19d1295f3c
     </div>
   );
 }
--- src/pages/admin/admin-create-voucher.jsx
+++ src/pages/admin/admin-create-voucher.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 // import SaveButton from "../../components/"
 import AdminSidebar from "../../components/admin-sidebar/admin-sidebar";
-<<<<<<< HEAD
 import AdminNavbar from "../../components/admin-navbar/admin-navbar";
-=======
->>>>>>> 920f0ff71a649808ebdb0fbdff658a19d1295f3c
 import "./admin-create-voucher.css";
 
 const AdminCreateVoucher = () => {
   return (
     <>
-<<<<<<< HEAD
       <AdminNavbar />
-      <body>
+      <div className="admin-container">
         <AdminSidebar />
         <div className="admin-create-content">
           <h1 className="admin-h1">Create New Vouchers</h1>
-=======
-      <body>
-        <AdminSidebar />
-        <div className="admin-create-content">
-          <h1>Create New Vouchers</h1>
->>>>>>> 920f0ff71a649808ebdb0fbdff658a19d1295f3c
           <input
             type="text"
             placeholder="Voucher Batch Name"
@@ -33,9 +23,9 @@ const AdminCreateVoucher = () => {
             className="create-voucher-batch-total"
             min="1"
           />
-          <button></button>
+          {/* <button>Create Vouchers</button> */}
         </div>
-      </body>
+      </div>
     </>
   );
 };
