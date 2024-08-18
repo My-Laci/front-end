da1ff4abbf889aba3a32b9ac5d3dee509a9083e3
moch-ilyas-saktiono-putra <moch.ilyas.saktiono-2021@fst.unair.ac.id>
2024-08-18 17:12:22 +0700

    fix styling and add post

--- src/components/profile-content/ProfileContent.jsx
+++ src/components/profile-content/ProfileContent.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
 import "../profile-content/ProfileContent.css";
 import Article from "../profile-article/ProfileArticle";
+import PostContent from "../post-content/post-content";
 
 export default function ProfileContent() {
   // State untuk melacak tab yang aktif
   const [activeTab, setActiveTab] = useState("posts");
+
+  // Data posts dan articles sebagai contoh
+  const posts = [
+    <>
+      <PostContent />
+    </>,
+  ];
+
+  // Array kosong untuk mensimulasikan tidak ada konten
+  const articles = [
+    <>
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+    </>,
+  ];
 
   // Handler untuk mengubah tab yang aktif
   const handleTabClick = (tab) => {
@@ -13,36 +32,38 @@ export default function ProfileContent() {
 
   return (
     <div className="profile-content">
-      <div className="navigation">
-        <div className="button-nest">
-          <span
-            href="#"
-            className={activeTab === "posts" ? "active" : ""}
-            onClick={() => handleTabClick("posts")}
-          >
-            Posts
-          </span>
+      <div className="profile-navigation">
+        <div
+          className={
+            activeTab === "posts"
+              ? "profile-button-nest active clickable"
+              : "profile-button-nest clickable"
+          }
+          onClick={() => handleTabClick("posts")}
+        >
+          <span>Posts</span>
         </div>
-        <div className="button-nest">
-          <span
-            href="#"
-            className={activeTab === "articles" ? "active" : ""}
-            onClick={() => handleTabClick("articles")}
-          >
-            Articles
-          </span>
+        <div
+          className={
+            activeTab === "articles"
+              ? "profile-button-nest active clickable"
+              : "profile-button-nest clickable"
+          }
+          onClick={() => handleTabClick("articles")}
+        >
+          <span>Articles</span>
         </div>
       </div>
       {activeTab === "articles" && (
-        <div className="content">
-          <Article />
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+        <div className="profile-content-list">
+          {articles.length > 0 ? articles : <p>Nothing to post</p>}
         </div>
       )}
-      {activeTab === "posts" && <div className="content"></div>}
+      {activeTab === "posts" && (
+        <div className="profile-content-list">
+          {posts.length > 0 ? posts : <p>Nothing to post</p>}
+        </div>
+      )}
     </div>
   );
 }
