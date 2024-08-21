d4d717f080ceb96540395feabd4166aa02c2f326
Adelnors <erlando.putra.21@gmail.com>
2024-08-21 20:58:47 +0700

    fix: add missing routes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -7,7 +7,14 @@ import AdminListVoucher from "../pages/admin/admin-list-voucher";
 import AdminListBatch from "../pages/admin/admin-list-batch";
 import Homepage from "../pages/homepage/homepage";
 import Popular from "../pages/popular/popular";
+import AccountInfoPage from "../pages/account-info-page/account-info-page.jsx";
+import ChangeNamePage from "../pages/change-name-page/change-name-page.jsx";
+import ChangeEmailPage from "../pages/change-email-page/change-email-page.jsx";
+import ChangePasswordPage from "../pages/change-password-page/change-password-page.jsx";
+import SearchResultPage from "../pages/search-result-page/search-result-page.jsx";
+import OtpEmailPage from "../pages/otp-email-page/otp-email-page.jsx";
 import NotFoundPage from "../pages/NotFound/NotFound.jsx";
+import AdminValidate from "../pages/admin/admin-validate.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -43,6 +50,34 @@ export const router = createBrowserRouter([
   {
     path: "/AdminListBatch",
     element: <AdminListBatch />,
+  },
+  {
+    path: "/AccountInfo",
+    element: <AccountInfoPage />,
+  },
+  {
+    path: "/ChangeName",
+    element: <ChangeNamePage />,
+  },
+  {
+    path: "/ChangeEmail",
+    element: <ChangeEmailPage />,
+  },
+  {
+    path: "/ChangePassword",
+    element: <ChangePasswordPage />,
+  },
+  {
+    path: "/EmailVerification",
+    element: <OtpEmailPage />,
+  },
+  {
+    path: "/SearchResult",
+    element: <SearchResultPage />,
+  },
+  {
+    path: "/AdminValidate",
+    element: <AdminValidate />,
   },
   {
     path: "*",
